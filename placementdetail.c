d5dfc783938f63d16c2e9516988617de97153763
siddarths1 <siddarths.20cse@kongu.edu>
2021-10-23 11:32:36 +0530

    placement commit

---
+++ placementdetail.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main()
+{
+int rollno,mobile,noofarre,home,gpa1,gpa2,gpa3,cgpa;
+string name,eduemail,peremail,address;
+
+ printf("enter the name");
+scanf("%s",name);
+printf("enter the rollno");
+scanf("%d",rollno);
+printf("enter mobile no");
+scanf("%d",mobile);
+printf("enter personal email");
+scanf("%s",email);
+printf("enter edu email");
+scanf("%s",eduemail);
+prinf("enter residential detail");
+scanf("%d",home);
+printf("enter no of arrears");
+scanf("%s",noofarre);
+printf("enter gpa 1");
+scanf("%d",gpa1);
+printf("enter gpa 2");
+scanf("%d",gpa2);
+printf("enter gpa 3");
+scanf("%d",gpa3);
+printf("enter cgpa");
+scanf("%d",cgpa);
+
+}
