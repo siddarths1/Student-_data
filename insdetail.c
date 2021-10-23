cf236dfebbc78a17c499e390438d28c090832f99
siddarths1 <siddarths.20cse@kongu.edu>
2021-10-23 11:15:42 +0530

    insurance commit

---
+++ insdetail.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void main()
+{
+int rollno,age,mobile,aadharno;
+string name,nomineename,relnomname,email,address,occ;
+
+ printf("enter the name");
+scanf("%s",name);
+printf("enter nominee name");
+scanf("%s",nomineename);
+printf("enter relationship of nominee name");
+scanf("%s",relnomname);
+printf("enter age");
+scanf("%d",age);
+printf("enter mobile no");
+scanf("%d",mobile);
+printf("enter email");
+scanf("%s",email);
+printf("enter occupation");
+scanf("%s",occ);
+printf("enter address");
+scanf("%s",address);
+printf("enter aadhar number");
+scanf("%d",aadharno);
+}
