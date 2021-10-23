cfd887ede81e32bbd0002f1f23104a36f345bb9b
siddarths1 <siddarths.20cse@kongu.edu>
2021-10-23 11:05:02 +0530

    first commit

---
+++ details.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void main()
+{
+int rollno,age,mobile,bloodgroup,aadharno;
+string name,fathername,mothername,email,address;
+
+ printf("enter the name");
+scanf("%s",name);
+printf("enter the rollno");
+scanf("%d",rollno);
+printf("enter father name");
+scanf("%s",fathername);
+printf("enter mother name");
+scanf("%s",mothername);
+printf("enter age");
+scanf("%d",age);
+printf("enter mobile no");
+scanf("%d",mobile);
+printf("enter email");
+scanf("%s",email);
+printf("enter blood group");
+scanf("%d",bloodgroup);
+printf("enter address");
+scanf("%s",address);
+printf("enter aadhar number");
+scanf("%d",aadharno);
+}
