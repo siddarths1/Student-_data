d436d76ebb1fa8c2b1e95fcc78af99a6901fb607
siddarths1 <siddarths.20cse@kongu.edu>
2021-10-25 19:36:27 +0530

    course complete

---
+++ coursedetails.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+ void main()
+{ 
+  int onecredit,valueadded,semester,nptel;
+  String rollno, namone[], namval[];
+  printf("enter roll no");
+  scanf("%s",rollno);
+  printf("enter semester");
+  scanf("%d",semester);
+  printf("enter no of one credit course");
+  scanf("%d",onecredit);
+  printf("enter name of one credit course");
+  scanf("%s",namone[]);
+  printf("enter no of value added course");
+  scanf("%d",valueadded);
+  printf("enter name of value added course");
+  scanf("%s",namval[]);
+  printf("enter nptel result");
+  scanf("%d",nptel);
+}
+
+
+
+
+
+
+
