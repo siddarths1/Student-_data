fe46cce9011c859964b53d9f437cf833af2d68b5
siddarths1 <siddarths.20cse@kongu.edu>
2021-10-25 19:23:10 +0530

    activity commit

---
+++ activitydetail.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+ 
+  int paper,project,papersout,projectsout,codecar,hackathon,sem;
+  String rollno;
+  printf("enter roll no");
+  scanf("%s",rollno);
+  printf("enter semester");
+  scanf("%d",sem);
+  printf("enter no of papers inside kec");
+  scanf("%d",paper);
+  printf("enter no of papers out");
+  scanf("%d",papersout);
+  printf("enter no of projects inside kec");
+  scanf("%d",projects);
+  printf("enter no of projects out");
+  scanf("%d",projectsout);
+  printf("no of prize in coding carnival");
+  scanf("%d",codecar);
+  printf("prize in hackathon");
+  scanf("%d",hackathon);
+
+}
+
